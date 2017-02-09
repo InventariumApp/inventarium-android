7eeda7e7e2f3e1e7cf64150eb4374740df5b94e6
Israel Hill <idh@case.edu>
2017-02-09 15:50:31 -0500

    configured firebase

--- app/src/main/java/com/inventariumapp/inventarium/MainActivity.java
+++ app/src/main/java/com/inventariumapp/inventarium/MainActivity.java
@@ -3,11 +3,17 @@ package com.inventariumapp.inventarium;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("testingDB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        myRef.setValue("Android", "Test");
     }
 }

42ea1a7eec666ed85bdacc21d4a81b1546fb213d
Yousef Mahmoud <Yousef Mahmoud>
2017-02-18 16:31:13 -0500

    Basic shopping list view logic

--- app/src/main/java/com/inventariumapp/inventarium/MainActivity.java
+++ app/src/main/java/com/inventariumapp/inventarium/MainActivity.java
@@ -2,11 +2,22 @@ package com.inventariumapp.inventarium;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> shoppingList = null;
+    ArrayAdapter<String> adapter = null;
+    ListView lv = null;
+
+
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("testingDB");
 
@@ -15,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        myRef.setValue("Android", "Test");
+
+        shoppingList = new ArrayList<>();
+        Collections.addAll(shoppingList, "Eggs", "Yogurt", "Milk", "Bananas", "Apples", "Tide with bleach", "Cascade");
+        shoppingList.addAll(Arrays.asList("Napkins", "Dog food", "Chapstick", "Bread"));
+        shoppingList.add("Sunscreen");
+        shoppingList.add("Toothpaste");
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, shoppingList);
+        lv = (ListView) findViewById(R.id.pantry_content);
+        lv.setAdapter(adapter);
     }
 }

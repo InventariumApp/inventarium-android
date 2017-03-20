7019a4a3bdbf497b207d0b210778507b3b31e1d2
Yousef Mahmoud <yfm@case.edu>
2017-03-20 15:27:03 -0400

    Duplicated Pantry work into shopping list

--- app/src/main/java/com/inventariumapp/inventarium/Fragments/ShoppingList.java
+++ app/src/main/java/com/inventariumapp/inventarium/Fragments/ShoppingList.java
@@ -2,6 +2,8 @@ package com.inventariumapp.inventarium.Fragments;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.inventariumapp.inventarium.R;
+import com.inventariumapp.inventarium.Utility.ItemAdapter;
+import com.inventariumapp.inventarium.Utility.ItemHandler;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,25 +23,26 @@ import java.util.Collections;
  */
 
 public class ShoppingList extends Fragment {
-    ArrayList<String> shoppingList = null;
-    ArrayAdapter adapter = null;
-    ListView lv = null;
+    RecyclerView mRecyclerView;
 
     @Override
-    // fragment to draw its user interface for the first time.
+    // Returns a View from this method that is the root of your fragment's layout
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState ) {
+                             Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.tab2_shopping_list, container, false);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.shopping_list_recycler_view);
 
-        shoppingList = new ArrayList<>();
-        Collections.addAll(shoppingList, "Eggs", "Yogurt", "Milk", "Bananas", "Apples", "Tide with bleach", "Cascade");
-        shoppingList.addAll(Arrays.asList("Napkins", "Dog food", "Chapstick", "Bread"));
-        shoppingList.add("Sunscreen");
-        shoppingList.add("Toothpaste");
-        adapter = new ArrayAdapter(container.getContext(), android.R.layout.simple_list_item_1, shoppingList);
-        lv = (ListView) rootView.findViewById(R.id.shopping_content);
-        lv.setAdapter(adapter);
+        // Set Up recycler view
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        mRecyclerView.setAdapter(new ItemAdapter());
+        mRecyclerView.setLayoutManager(llm);
+        mRecyclerView.setHasFixedSize(true);
+
+        // Set up animations and touch events
+        ItemHandler itemHandler = new ItemHandler(mRecyclerView, getActivity());
+        itemHandler.setUpItemTouchHelper();
+        itemHandler.setUpAnimationDecoratorHelper();
         return rootView;
     }
 }

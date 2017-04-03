e4e5c6ab819e88d3d1c4009ded11a9dfd549a1f0
Yousef Mahmoud <yfm@case.edu>
2017-04-03 01:15:16 -0400

    can now upload images to storage

--- app/src/main/java/com/inventariumapp/inventarium/Activities/MainActivity.java
+++ app/src/main/java/com/inventariumapp/inventarium/Activities/MainActivity.java
@@ -2,6 +2,9 @@ package com.inventariumapp.inventarium.Activities;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
@@ -12,6 +15,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -20,17 +24,23 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.zxing.client.android.CaptureActivity;
 import com.inventariumapp.inventarium.Fragments.Pantry;
 import com.inventariumapp.inventarium.Fragments.ShoppingList;
 import com.inventariumapp.inventarium.R;
 import android.Manifest;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -177,7 +187,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBarcodeClick(View v) {
-
         // Check for the camera permission before accessing the camera.  If the
         // permission is not granted yet, request permission.
         int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
@@ -186,6 +195,21 @@ public class MainActivity extends AppCompatActivity {
         } else {
             requestCameraPermission();
         }
+    }
+
+    public void onImageClick(View v) {
+        // Check for the camera permission before accessing the camera.  If the
+        // permission is not granted yet, request permission.
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, 2);
+            }
+        } else {
+            requestCameraPermission();
+        }
+
     }
 
     private void requestCameraPermission() {
@@ -226,9 +250,37 @@ public class MainActivity extends AppCompatActivity {
                 String contents = data.getStringExtra("SCAN_RESULT");
                 String format = data.getStringExtra("SCAN_RESULT_FORMAT");
             } else if (resultCode == RESULT_CANCELED) {
-// Handle cancel
+            // Handle cancel
             }
         }
+        if (requestCode == 2) {
+            encodeBitmapAndSaveToFirebase(data);
+        }
+    }
+
+    public void encodeBitmapAndSaveToFirebase(Intent data) {
+        //saves the pic locally
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] dataBAOS = baos.toByteArray();
+
+        // Create a storage reference from our app
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imagesRef = storageRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("image");
+        UploadTask uploadTask = imagesRef.putBytes(dataBAOS);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+            }
+        });
     }
 
 }
